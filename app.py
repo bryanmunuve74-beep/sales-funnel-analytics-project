c3ccec0df09002037ac827012b85ee031d4191d4
bryan-munuve-data <bryanmunuve74@gmail.com>
2026-02-11 16:28:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from datetime import datetime, timedelta
+import matplotlib
+import matplotlib.pyplot as plt
+
+
 
 # -----------------------------
 # PAGE CONFIG
